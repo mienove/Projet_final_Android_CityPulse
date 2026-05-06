c34b7625925d7db69fae2d124f1744a2fc83765b
Solem <onesoloday26@gmail.com>
2026-05-06 07:04:00 -0400

    Corection d'erreur

--- app/src/main/java/com/example/citypulse/viewmodel/LieuxViewModel.kt
+++ app/src/main/java/com/example/citypulse/viewmodel/LieuxViewModel.kt
@@ -2,8 +2,10 @@ package com.example.citypulse.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.citypulse.data.local.LieuxDAO
 import com.example.citypulse.model.Lieux
+import kotlinx.coroutines.launch
 
 class LieuxViewModel (
     private val daolieu: LieuxDAO
@@ -12,7 +14,7 @@ class LieuxViewModel (
 //    charger les lieux
     fun loadLieux(){
         viewModelScope.launch{
-            lieu.value = daolieu.getAllLieux()
+            lieu.value = daolieu.getAllLieux() as List<Lieux>?
         }
     }
 }
